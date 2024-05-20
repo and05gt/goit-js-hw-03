ab14f5e4c19afdd7638f3eb4012a8e2281eb1f33
Andrii D. <and05gt@gmail.com>
2024-05-20 20:03:06 +0200

    Add index and js files

---
+++ js/task-2.js
@@ -0,0 +1,17 @@
+function makeArray(firstArray, secondArray, maxLength) {
+  const totalArray = firstArray.concat(secondArray);
+  let newArray = [];
+  for (let i = 0; i < totalArray.length; i += 1) {
+    if (i < maxLength) {
+      newArray.push(totalArray[i]);
+    }
+  }
+  return newArray;
+}
+
+console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
+console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
